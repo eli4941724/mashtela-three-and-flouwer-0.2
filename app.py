5ac8dc8962c1be659b7e25870212be8e35dd272b
eli4941724 <eleanordegani@gmail.com>
2025-07-31 11:17:09 +0000

    הוספת form.html.jinja2 ותיקון app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect # type: ignore
 import os
 import pandas as pd
 import requests
